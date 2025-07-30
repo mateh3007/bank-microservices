0595d1a3c2475b0dfc9c63b819e946f3387ba576
Matheus <mateh11233@gmail.com>
2025-07-30 12:19:07 -0300

    feat: :sparkles: add initial migration and schema for transactions

---
+++ transactions/prisma/migrations/20250730143133_initial_config_v1/migration.sql
@@ -0,0 +1,16 @@
+-- CreateEnum
+CREATE TYPE "public"."TransactionStatus" AS ENUM ('pending', 'completed', 'failed');
+
+-- CreateTable
+CREATE TABLE "public"."transactions" (
+    "id" TEXT NOT NULL,
+    "senderId" TEXT NOT NULL,
+    "receiverId" TEXT NOT NULL,
+    "amount" DOUBLE PRECISION NOT NULL,
+    "description" TEXT,
+    "status" "public"."TransactionStatus" NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "transactions_pkey" PRIMARY KEY ("id")
+);
