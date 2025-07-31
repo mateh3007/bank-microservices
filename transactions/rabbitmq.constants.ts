d175c8714fefef37c5d124be955d1d00cd042aa9
Matheus <mateh11233@gmail.com>
2025-07-31 13:15:37 -0300

    chore: :fire: remove unused files

--- transactions/rabbitmq.constants.ts
+++
@@ -1,6 +0,0 @@
-export const RABBITMQ_OPTIONS = {
-  urls: [process.env.RABBITMQ_URL || 'amqp://guest:guest@rabbitmq:5672'],
-  queueTransactions: 'transactions_queue',
-  exchangeTransactions: 'transactions_exchange',
-  routingKeyCreated: 'transaction.created',
-};
