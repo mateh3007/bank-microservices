1886ff956127b13999190f23224178cd52e4f815
Matheus <mateh11233@gmail.com>
2025-07-31 20:47:02 -0300

    chore: :card_file_box: database changes and dependencies updating

--- transactions/prisma/migrations/20250731230004_initial_config_v1/migration.sql
+++ transactions/prisma/migrations/20250731230004_initial_config_v1/migration.sql
@@ -6,7 +6,7 @@ CREATE TABLE "public"."transactions" (
     "id" TEXT NOT NULL,
     "senderId" TEXT NOT NULL,
     "receiverId" TEXT NOT NULL,
-    "amount" BIGINT NOT NULL,
+    "amount" INTEGER NOT NULL,
     "description" TEXT,
     "status" "public"."TransactionStatus" NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
